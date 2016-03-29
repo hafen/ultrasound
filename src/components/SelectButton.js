3cf3f32b373fef630a99632615c05c587fcf1bef
hafen <rhafen@gmail.com>
2016-03-29 01:41:58 -0700

    Remove material-ui

--- src/components/SelectButton.js
+++ src/components/SelectButton.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import { FlatButton } from 'material-ui';
-
-const style = {
-  // border: '1px solid #aaa',
-  minWidth: 40,
-  color: 'rgb(0, 188, 212)'
-};
 
 const SelectButton = ({ selected, val, clickHandler }) => {
-  const labelStyle = { color: 'rgb(0, 188, 212)' };
-  const backgroundColor = selected ? 'rgb(0, 188, 212)' : 'white';
-  const hoverColor = selected ? 'rgb(0, 188, 212)' : '#eee';
-  labelStyle.color = selected ? 'white' : 'rgb(0, 188, 212)';
+  const style = {
+    minWidth: 40,
+    border: 10,
+    boxSizing: 'border-box',
+    display: 'inline-block',
+    cursor: 'pointer',
+    transform: 'translate3d(0px, 0px, 0px)',
+    transition: 'all 450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+    fontSize: 14,
+    borderRadius: 2,
+    position: 'relative',
+    lineHeight: '36px',
+    textAlign: 'center',
+    backgroundColor: selected ? 'rgb(0, 188, 212)' : 'white',
+    color: selected ? 'white' : 'rgb(0, 188, 212)'
+  };
+
   return (
-  <FlatButton
+  <div
     primary
     style = {style}
-    labelStyle = {labelStyle}
-    backgroundColor = {backgroundColor}
-    hoverColor = {hoverColor}
-    label = {val}
     onClick = {() => { clickHandler(val); }}
-  />);
+  >{val}</div>
+  );
 };
 
 SelectButton.propTypes = {
